d8a69d0bd2a31ef39ba42e0bad5f7a28219487ac
darrenpaul <drobertpaul@gmail.com>
2022-12-20 18:18:06 +0200

    fixed background and icons

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -2,8 +2,8 @@
   <section class="project-section">
     <h1>{{ project.title }}</h1>
     <div class="project-image">
-      <iframe v-if="project.liveUrl" class="w-full h-full" :src="project.liveUrl" />
-      <img v-else class="w-full h-full" :src="project.projectImages[imageIndex]" />
+      <iframe v-if="project.liveUrl" :src="project.liveUrl" />
+      <img v-else :src="project.projectImages[imageIndex]" />
 
       <div v-if="project.liveUrl === null && project.projectImages.length > 1" class="project-action-buttons">
         <button class="project-action-button" @click="onArrowLeftClick">
@@ -15,18 +15,18 @@
       </div>
     </div>
 
-    <div class="grid grid-cols-2 gap-4 mt-4">
-      <div class="border-double border-8">
-        <div class="bg-night-400 p-8 w-full">
+    <div class="project-details">
+      <div class="project-card">
+        <div class="project-content">
           <SanityContent :blocks="project.description" :serializers="serializers" />
         </div>
       </div>
-      <div class="border-double border-8">
-        <div class="bg-night-400 p-8 flex flex-wrap gap-4">
+      <div class="project-card">
+        <div class="project-frameworks">
           <img
             v-for="(framework, index) in project.frameworks"
             :key="index"
-            class="w-auto h-12"
+            class="project-framework-logo"
             :src="FRAMEWORK_LOGOS[framework]?.logo"
           />
         </div>
