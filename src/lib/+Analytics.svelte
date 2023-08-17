39a14ad11def8946846a03f8cae7f1a526ada763
Darren Paul <drobertpaul@gmail.com>
2023-08-17 19:10:51 +0200

    added sitemap

---
+++ src/lib/+Analytics.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { PUBLIC_SANITY_PROJECT_ID } from '$env/static/public';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', PUBLIC_SANITY_PROJECT_ID, {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${PUBLIC_SANITY_PROJECT_ID}`}>
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', PUBLIC_SANITY_PROJECT_ID);
+	</script>
+</svelte:head>
