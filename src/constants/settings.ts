41ad762ace319530c46fe1e4db0a570a8bde0807
Darren <drobertpaul@gmail.com>
2026-04-08 21:57:28 +0100

    Add posthog

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -2,4 +2,6 @@ export default {
   domain: import.meta.env.PUBLIC_DOMAIN,
   siteUrl: import.meta.env.PUBLIC_SITE_URL,
   siteName: import.meta.env.PUBLIC_SITE_NAME,
+  posthogKey: import.meta.env.PUBLIC_POSTHOG_KEY,
+  posthogHost: import.meta.env.PUBLIC_POSTHOG_HOST ?? 'https://us.i.posthog.com',
 };
